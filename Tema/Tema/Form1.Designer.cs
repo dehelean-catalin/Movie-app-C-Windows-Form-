858f543c6cf2824d2da0f02f9ca8e72f2740af2c
Dehelean Rares Catalin <drcatalin00@gmail.com>
2022-03-27 20:19:26 +0300

    mesaj

---
+++ Tema/Tema/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace Tema
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.genreBox = new System.Windows.Forms.ComboBox();
+            this.aboutBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.ratingBox = new System.Windows.Forms.TextBox();
+            this.titleBox = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnAdd.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAdd.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.ForeColor = System.Drawing.SystemColors.Info;
+            this.btnAdd.Location = new System.Drawing.Point(87, 459);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(137, 33);
+            this.btnAdd.TabIndex = 11;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnUpdate.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnUpdate.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.ForeColor = System.Drawing.SystemColors.Info;
+            this.btnUpdate.Location = new System.Drawing.Point(230, 459);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(136, 33);
+            this.btnUpdate.TabIndex = 12;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Red;
+            this.btnDelete.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDelete.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.ForeColor = System.Drawing.SystemColors.Info;
+            this.btnDelete.Location = new System.Drawing.Point(88, 498);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(136, 33);
+            this.btnDelete.TabIndex = 13;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnClear.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClear.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.ForeColor = System.Drawing.SystemColors.Info;
+            this.btnClear.Location = new System.Drawing.Point(230, 498);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(136, 33);
+            this.btnClear.TabIndex = 14;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // genreBox
+            // 
+            this.genreBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.genreBox.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.genreBox.FormattingEnabled = true;
+            this.genreBox.Items.AddRange(new object[] {
+            "Action",
+            "Animation",
+            "Drama",
+            "Horror",
+            "Sci-Fi",
+            "Thriller"});
+            this.genreBox.Location = new System.Drawing.Point(92, 192);
+            this.genreBox.Name = "genreBox";
+            this.genreBox.Size = new System.Drawing.Size(274, 31);
+            this.genreBox.TabIndex = 18;
+            // 
+            // aboutBox
+            // 
+            this.aboutBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.aboutBox.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aboutBox.Location = new System.Drawing.Point(172, 280);
+            this.aboutBox.Multiline = true;
+            this.aboutBox.Name = "aboutBox";
+            this.aboutBox.Size = new System.Drawing.Size(194, 100);
+            this.aboutBox.TabIndex = 17;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Cambria", 14F);
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label7.Location = new System.Drawing.Point(87, 280);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(74, 28);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "About";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Cambria", 14F);
+            this.label8.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label8.Location = new System.Drawing.Point(87, 237);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(79, 28);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Rating";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Cambria", 14F);
+            this.label9.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label9.Location = new System.Drawing.Point(87, 161);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(74, 28);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Genre";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Font = new System.Drawing.Font("Cambria", 14F);
+            this.label10.ForeColor = System.Drawing.SystemColors.Info;
+            this.label10.Location = new System.Drawing.Point(87, 96);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(60, 28);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "Title";
+            // 
+            // ratingBox
+            // 
+            this.ratingBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.ratingBox.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ratingBox.Location = new System.Drawing.Point(172, 237);
+            this.ratingBox.Name = "ratingBox";
+            this.ratingBox.Size = new System.Drawing.Size(194, 31);
+            this.ratingBox.TabIndex = 12;
+            // 
+            // titleBox
+            // 
+            this.titleBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.titleBox.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleBox.Location = new System.Drawing.Point(92, 127);
+            this.titleBox.Name = "titleBox";
+            this.titleBox.Size = new System.Drawing.Size(274, 31);
+            this.titleBox.TabIndex = 11;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(985, -13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(133, 118);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 19;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(405, 127);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(646, 443);
+            this.dataGridView1.TabIndex = 20;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Info;
+            this.button1.Location = new System.Drawing.Point(88, 537);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(278, 33);
+            this.button1.TabIndex = 23;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Info;
+            this.label1.Location = new System.Drawing.Point(412, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(253, 55);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Movie app";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1105, 582);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.genreBox);
+            this.Controls.Add(this.aboutBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.ratingBox);
+            this.Controls.Add(this.titleBox);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.ComboBox genreBox;
+        private System.Windows.Forms.TextBox aboutBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox ratingBox;
+        private System.Windows.Forms.TextBox titleBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Tema/Tema/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Tema
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        DataTable table = new DataTable();
+        int index;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            table.Columns.Add("Movie Title", typeof(string));
+            table.Columns.Add("Gender", typeof (string));
+            table.Columns.Add("My rating", typeof (double));
+            table.Columns.Add("About", typeof(string));
+
+            dataGridView1.DataSource = table;
+
+            string[] lines = File.ReadAllLines(@"C:\Users\Eu\OneDrive\Desktop\Tema\Tema\bin\Debug\fisier.txt");
+            string[] values;
+
+            for(int i = 0; i < lines.Length; i++)
+            {
+                values = lines[i].ToString().Split(',');
+                string[] row = new string[values.Length];
+
+                for(int j=0; j < values.Length; j++)
+                {
+                    row[j] = values[j].Trim();
+                }
+
+                table.Rows.Add(row);
+            }
+            
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            double i;
+            if(titleBox.Text != "" && genreBox.Text != "" && double.TryParse(ratingBox.Text, out i) && aboutBox.Text !="" )
+            {
+               table.Rows.Add(titleBox.Text, genreBox.Text, ratingBox.Text, aboutBox.Text);
+                clearFields();
+            } else
+            {
+                MessageBox.Show("Invalid input");
+            }
+            
+        }
+          private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            index = e.RowIndex;
+            DataGridViewRow row = dataGridView1.Rows[index];
+            
+            titleBox.Text = row.Cells[0].Value.ToString();
+            genreBox.Text = row.Cells[1].Value.ToString();
+            ratingBox.Text = row.Cells[2].Value.ToString();
+            aboutBox.Text = row.Cells[3].Value.ToString();
+
+        }
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            double i;
+            DataGridViewRow newdata = dataGridView1.Rows[index];
+            if (titleBox.Text != "" && genreBox.Text != "" && double.TryParse(ratingBox.Text, out i) && aboutBox.Text != "")
+            {
+              newdata.Cells[0].Value = titleBox.Text;
+              newdata.Cells[1].Value = genreBox.Text;
+              newdata.Cells[2].Value = ratingBox.Text;
+              newdata.Cells[3].Value = aboutBox.Text;
+              clearFields();
+            }else
+            {
+                MessageBox.Show("You didnt select a movie");
+            }
+                
+        }
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string message = "Do you want to delete this movie";
+            string title = "Delete the list";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result = MessageBox.Show(message, title, buttons);
+            if (result == DialogResult.Yes)
+            {
+                index = dataGridView1.CurrentCell.RowIndex;
+            dataGridView1.Rows.RemoveAt(index);
+            clearFields();
+            }
+            
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            string message = "Do you want to delete the entire list";
+            string title = "Delete the list";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result = MessageBox.Show(message, title, buttons);
+            if (result == DialogResult.Yes)
+            {
+                table.Clear();
+            }
+            
+        }
+
+       
+        public void clearFields()
+        {
+            titleBox.Text=" ";
+            genreBox.Text = "";
+            ratingBox.Text = "";
+            aboutBox.Text = "";
+        }
+
+       
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+            string message = "Do you want to exit?";
+            string title = "Delete the list";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result = MessageBox.Show(message, title, buttons);
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamWriter writer = new StreamWriter(@"C:\Users\Eu\OneDrive\Desktop\Tema\Tema\bin\Debug\fisier.txt");
+            
+            for(int i = 0; i< dataGridView1.Rows.Count-1; i++)
+            {
+                writer.Write( dataGridView1.Rows[i].Cells[0].Value.ToString() + ',');
+                writer.Write( dataGridView1.Rows[i].Cells[1].Value.ToString() + ',');
+                writer.Write( dataGridView1.Rows[i].Cells[2].Value.ToString() + ',');
+                writer.WriteLine( dataGridView1.Rows[i].Cells[3].Value.ToString() );
+
+            }
+            writer.Close();
+        }
+    }
+   
+}
